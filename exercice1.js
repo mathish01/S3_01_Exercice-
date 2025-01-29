484ad6f4f26a26dbdfc77aa6467e4b6b6d321d7b
Mathis Holler <m.holler@orange.fr>
2025-01-29 12:33:11 +0100

    resolution exercice 1

---
+++ exercice1.js
@@ -0,0 +1,18 @@
+// Nous souhaitons faire la somme du prix de plusieurs articles
+// Prix de deux articles
+let article1 = 10;
+let article2 = 20;
+// Nous calculons le total
+let total = article1 + article2;
+// Prix d'article2
+// article
+let article3 = 50
+// Nous ajoutons article3 au totalal
+let totalAvecReduction = total - total * 0.2;  
+
+totalAvecReduction = total + article3;
+
+console.log("Le prix total avant reduction est de : ", total);
+console.log("Le prix total après reduction est de : ", totalAvecReduction);
+
+//Nous appliquons 20% de reduction
